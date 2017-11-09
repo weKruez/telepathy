151fc636c08f0f15868bc9e8b7e776366f703c3a
Christian Weiss <wekruez@protonmail.com>
2017-11-08 20:21:33 -0500

    Telepathy Reader based on base ten of zenner cards. Must be played with two people. Future iterations will have telepathy reading against computer.

---
+++ telepathy.java
@@ -0,0 +1,359 @@
+
+//ten trials
+
+import javax.swing.JOptionPane;
+
+public class telepathy {
+    
+    public static void main(String args []) {
+        
+        
+        JOptionPane.showMessageDialog(null, "It takes two people to read telepathy. Have one person pick a variable. Then leave the screen to see if the variable is predicted accurately.");
+        
+        double percent;
+        String card_one;
+        String card_two;
+        String card_three;
+        String card_four;
+        String card_five;
+        String card_six;
+        String card_seven;
+        String card_eight;
+        String card_nine;
+        String card_ten;
+        String card_one_p;
+        String card_two_p;
+        String card_three_p;
+        String card_four_p;
+        String card_five_p;
+        String card_six_p;
+        String card_seven_p;
+        String card_eight_p;
+        String card_nine_p;
+        String card_ten_p;
+        
+        //card 1
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_one = IO.readString();
+        
+        while(!card_one.equals("!") && !card_one.equals("@") && !card_one.equals("#") && !card_one.equals("$") && !card_one.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_one = IO.readString();
+            continue;
+        }
+        //card 1 p
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_one_p = IO.readString();
+        
+        while(!card_one_p.equals("!") && !card_one_p.equals("@") && !card_one_p.equals("#") && !card_one_p.equals("$") && !card_one_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_one = IO.readString();
+            continue;
+        }
+        //card 2
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_two = IO.readString();
+        
+        while(!card_two.equals("!") && !card_two.equals("@") && !card_two.equals("#") && !card_two.equals("$") && !card_two.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_two = IO.readString();
+            continue;
+        }
+        //card 2 p
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_two_p = IO.readString();
+        
+        while(!card_two_p.equals("!") && !card_two_p.equals("@") && !card_two_p.equals("#") && !card_two_p.equals("$") && !card_two_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_three = IO.readString();
+            continue;
+        }
+        //card 3
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_three = IO.readString();
+        
+        while(!card_three.equals("!") && !card_three.equals("@") && !card_three.equals("#") && !card_three.equals("$") && !card_three.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_three = IO.readString();
+            continue;
+        }
+        
+        //card 3 p
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_three_p = IO.readString();
+        
+        while(!card_three_p.equals("!") && !card_three_p.equals("@") && !card_three_p.equals("#") && !card_three_p.equals("$") && !card_three_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_three_p = IO.readString();
+            continue;
+        }
+        
+        //card 4
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_four = IO.readString();
+        
+        while(!card_four.equals("!") && !card_four.equals("@") && !card_four.equals("#") && !card_four.equals("$") && !card_four.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_four = IO.readString();
+            continue;
+        }
+        
+        //card 4 p
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_four_p = IO.readString();
+        
+        while(!card_four_p.equals("!") && !card_four_p.equals("@") && !card_four_p.equals("#") && !card_four_p.equals("$") && !card_four_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_four_p = IO.readString();
+            continue;
+        }
+        
+        //card 5
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_five = IO.readString();
+        
+        while(!card_five.equals("!") && !card_five.equals("@") && !card_five.equals("#") && !card_five.equals("$") && !card_five.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_five = IO.readString();
+            continue;
+        }
+        
+        //card 5 p
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_five_p = IO.readString();
+        
+        while(!card_five_p.equals("!") && !card_five_p.equals("@") && !card_five_p.equals("#") && !card_five_p.equals("$") && !card_five_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_five_p = IO.readString();
+            continue;
+        }
+        
+        //card 6
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_six = IO.readString();
+        
+        while(!card_six.equals("!") && !card_six.equals("@") && !card_six.equals("#") && !card_six.equals("$") && !card_six.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_six = IO.readString();
+            continue;
+        }
+        
+        //6 p
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_six_p = IO.readString();
+        
+        while(!card_six_p.equals("!") && !card_six_p.equals("@") && !card_six_p.equals("#") && !card_six_p.equals("$") && !card_six_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_six_p = IO.readString();
+            continue;
+        }
+        
+        //card 7
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_seven = IO.readString();
+        
+        while(!card_seven.equals("!") && !card_seven.equals("@") && !card_seven.equals("#") && !card_seven.equals("$") && !card_seven.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_seven = IO.readString();
+            continue;
+        }
+        
+        // card 7 p
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_seven_p = IO.readString();
+        
+        while(!card_seven_p.equals("!") && !card_seven_p.equals("@") && !card_seven_p.equals("#") && !card_seven_p.equals("$") && !card_seven_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_seven_p = IO.readString();
+            continue;
+        }
+        
+        // card 8
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_eight = IO.readString();
+        
+        while(!card_eight.equals("!") && !card_eight.equals("@") && !card_eight.equals("#") && !card_eight.equals("$") && !card_eight.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_eight = IO.readString();
+            continue;
+        }
+        
+        //card 8 p
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_eight_p = IO.readString();
+        
+        while(!card_eight_p.equals("!") && !card_eight_p.equals("@") && !card_eight_p.equals("#") && !card_eight_p.equals("$") && !card_eight_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_eight_p = IO.readString();
+            continue;
+        }
+        
+        // card 9
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_nine = IO.readString();
+        
+        while(!card_nine.equals("!") && !card_nine.equals("@") && !card_nine.equals("#") && !card_nine.equals("$") && !card_nine.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_nine = IO.readString();
+            continue;
+        }
+        
+        //card 9 p
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_nine_p = IO.readString();
+        
+        while(!card_nine_p.equals("!") && !card_nine_p.equals("@") && !card_nine_p.equals("#") && !card_nine_p.equals("$") && !card_nine_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_nine_p = IO.readString();
+            continue;
+        }
+        
+        // card 10
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_ten = IO.readString();
+        
+        while(!card_nine.equals("!") && !card_ten.equals("@") && !card_ten.equals("#") && !card_ten.equals("$") && !card_ten.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_ten = IO.readString();
+            continue;
+        }
+        
+        //card 10 p
+        
+        System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+        card_ten_p = IO.readString();
+        
+        while(!card_ten_p.equals("!") && !card_ten_p.equals("@") && !card_ten_p.equals("#") && !card_ten_p.equals("$") && !card_ten_p.equals("%")){
+            IO.reportBadInput();
+            System.out.println("Enter '!', '@', '#', '$' or '%' in CML.");
+            card_ten_p = IO.readString();
+            continue;
+        }
+        double counter = 0;
+        if(card_one.equals(card_one_p)){
+            counter += 10;
+        }
+        
+        if(card_two.equals(card_two_p)){
+            counter += 10;
+        }
+        
+        if(card_three.equals(card_three_p)){
+            counter += 10;
+        }
+        
+        if(card_four.equals(card_four_p)){
+            counter += 10;
+        }
+        
+        if(card_five.equals(card_two_p)){
+            counter += 10;
+        }
+        
+        if(card_six.equals(card_six_p)){
+            counter += 10;
+        }
+        
+        if(card_seven.equals(card_seven_p)){
+            counter += 10;
+        }
+        
+        if(card_eight.equals(card_eight_p)){
+            counter += 10;
+        }
+        
+        if(card_nine.equals(card_nine_p)){
+            counter += 10;
+        }
+        
+        if(card_ten.equals(card_ten_p)){
+            counter += 10;
+        }
+        
+        if(!card_one.equals(card_one_p)){
+            counter -= 10;
+        }
+        
+        if(!card_two.equals(card_two_p)){
+            counter -= 10;
+        }
+        
+        if(!card_three.equals(card_three_p)){
+            counter -= 10;
+        }
+        
+        if(!card_four.equals(card_four_p)){
+            counter -= 10;
+        }
+        
+        if(!card_five.equals(card_five_p)){
+            counter -= 10;
+        }
+        
+        if(!card_six.equals(card_six_p)){
+            counter -= 10;
+        }
+        
+        if(!card_seven.equals(card_seven_p)){
+            counter -= 10;
+        }
+        
+        if(!card_eight.equals(card_eight_p)){
+            counter -= 10;
+        }
+        
+        if(!card_nine.equals(card_nine_p)){
+            counter -= 10;
+        }
+        
+        if(!card_ten.equals(card_ten_p)){
+            counter -= 10;
+        }
+        
+        
+        percent = counter;
+        
+        System.out.println("You are " + percent + "% telepathic.");
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
